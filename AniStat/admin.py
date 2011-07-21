55d8d00d48aadb5ab492f1e8934c5e7dea8a5893
Paul Gulley <nano3.14@gmail.com>
2011-07-21 11:20:44 -0400

    fixed admin

---
+++ AniStat/admin.py
@@ -0,0 +1,7 @@
+from AniStat.models import Animal, FormCheck, FormDay, Profile
+from django.contrib import admin
+
+admin.site.register(Animal)
+admin.site.register(FormCheck)
+admin.site.register(FormDay)
+admin.site.register(Profile)
